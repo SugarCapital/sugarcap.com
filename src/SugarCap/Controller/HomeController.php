ebb93b51cf29f3359da85477c3168c71d24d1f64
briansugar@gmail.com <Sug@r925>
2020-10-04 15:53:56 -0700

    add gps and polyops

--- src/SugarCap/Controller/HomeController.php
+++ src/SugarCap/Controller/HomeController.php
@@ -17,8 +17,8 @@ class HomeController
     }
 
     public function __invoke(Request $request, Response $response, array $args): Response {
-        $templateArgs['companies'] = [
-            ['name' => 'Everlane', 'link' => 'https://www.everlane.com', 'img' => "/imgs/logos/everlane.jpg"],
+        $templateArgs['angel'] = [
+            ['name' => 'Everlane', 'link' => 'https://www.everlane.com', 'img' => "/imgs/logos/everlane.jpg" ],
             ['name' => 'Olive & June', 'link' => 'https://www.oliveandjune.com', 'img' => "/imgs/logos/olive-and-june.jpg"],
             ['name' => 'Afterpay', 'link' => 'https://www.afterpay.com', 'img' => "/imgs/logos/afterpay.jpg"],
             ['name' => 'Roadster', 'link' => 'https://www.roadster.com', 'img' => "/imgs/logos/roadster.jpg"],
@@ -35,21 +35,30 @@ class HomeController
             ['name' => 'Fast', 'link' => 'https://www.fast.co', 'img' => "/imgs/logos/fast.png"],
             ['name' => 'Optimizely', 'link' => 'https://www.optimizely.com', 'img' => "/imgs/logos/optimizely.jpg"],
             ['name' => 'Tracksmith', 'link' => 'https://www.tracksmith.com', 'img' => "/imgs/logos/tracksmith.jpg"],
+            ['name' => 'Foxeye', 'link' => 'https://www.foxeyerobotics.com', 'img' => "/imgs/logos/foxeye.png"],
+        ];
+        $templateArgs['fund1'] = [
+            ['name' => 'Black Wolf', 'link' => 'https://blackwolfnation.com', 'img' => "/imgs/logos/blackwolf.jpg"],
+            ['name' => 'Clyde', 'link' => 'https://www.joinclyde.com/', 'img' => "/imgs/logos/clyde.jpg"],
+            ['name' => 'Kinship', 'link' => 'https://lovekinship.com/', 'img' => "/imgs/logos/kinship.jpg"],
+            ['name' => 'Shopping Gives', 'link' => 'https://shoppinggives.com/', 'img' => "/imgs/logos/shopping-gives.jpg"],
+            // ['name' => 'co-op', 'link' => 'https://www.coopcommerce.co/', 'img' => "/imgs/logos/co-op.jpg"],
             ['name' => 'Brightland', 'link' => 'https://brightland.co', 'img' => "/imgs/logos/brightland.jpg"],
             ['name' => 'Builder.io', 'link' => 'https://builder.io', 'img' => "/imgs/logos/builderio.png"],
             ['name' => 'Jupiter', 'link' => 'https://hellojupiter.com', 'img' => "/imgs/logos/jupiter.png"],
             ['name' => 'Caraway', 'link' => 'https://www.carawayhome.com', 'img' => "/imgs/logos/caraway.jpg"],
             ['name' => 'Judy', 'link' => 'https://readyjudy.com', 'img' => "/imgs/logos/judy.jpg"],
-            ['name' => 'Foxeye', 'link' => 'https://www.foxeyerobotics.com', 'img' => "/imgs/logos/foxeye.png"],
-            ['name' => 'Black Wolf', 'link' => 'https://blackwolfnation.com', 'img' => "/imgs/logos/blackwolf.jpg"],
-            ['name' => 'Clyde', 'link' => 'https://www.joinclyde.com/', 'img' => "/imgs/logos/clyde.jpg"],
-            ['name' => 'Kinship', 'link' => 'https://lovekinship.com/', 'img' => "/imgs/logos/kinship.jpg"],
-            ['name' => 'Shopping Gives', 'link' => 'https://shoppinggives.com/', 'img' => "/imgs/logos/shopping-gives.jpg"],
+            ['name' => 'Polyops', 'link' => 'https://polyoperations.com', 'img' => "/imgs/logos/polyops.png"],
         ];
 
-        usort($templateArgs['companies'], function($a, $b) {
+        usort($templateArgs['angel'], function($a, $b) {
             return strcmp($a["name"], $b["name"]);
         });
+
+        usort($templateArgs['fund1'], function($a, $b) {
+            return strcmp($a["name"], $b["name"]);
+        });
+
         $args['body'] = $this->renderer->render('home', $templateArgs);
         return $response->write($this->renderer->render('index', $args));
     }
--- templates/home.tpl
+++ templates/home.tpl
@@ -5,25 +5,61 @@
       <div class="pt-4 my-4">
         <a href="https://www.instagram.com/sugarcapital/following/"><img src="/imgs/sugar-capital-red.png" width="300" width="auto" /></a>
       </div>
-
       <h1 class='text-display text-white pt-4 py-2 pb-4'>A Venture Capital Firm</h1>
       <h2 class='text-serif text-white pb-1'>Sugar Capital invests in smart companies that seek to elevate everyday life.</h2>
     </div>
     <div class="py-4"></div>
-    <div class="py-4"></div>
+    <div class="py-4">
+    </div>
   </div>
 </div>
-<div class="bg-tertiary py-4">
+<div class="bg-tertiary py-5">
   <div class="container">
-    <div class="text-secondary">
-      <h3>Companies</h3>
+  <div class="text-secondary">
+      <h3>General Partners</h3>
     </div>
     <div class="d-flex flex-wrap">
-      <?php foreach ($companies as $company) { ?>
+      <div class="text-sans-serif partner-logo text-center m-1 m-lg-4">
+        <a href="https://www.linkedin.com/in/will-hawthorne-1336a72/" class="">
+          <div  class="icon" style="background-image:url('/imgs/partners/will.png')"></div>
+          <div class="text-center pt-2 text-primary">Will Hawthorne</div>
+        </a>
+      </div>
+      <div class="text-sans-serif partner-logo text-center m-1 m-lg-4">
+        <a href="https://www.linkedin.com/in/krista-moatz-a1ab8a23/" class="">
+          <div  class="icon" style="background-image:url('/imgs/partners/krista.png')"></div>
+          <div class="text-center pt-2 text-primary">Krista Moatz</div>
+        </a>
+      </div>
+      <div class="text-sans-serif partner-logo text-center m-1 m-lg-4">
+        <a href="https://www.linkedin.com/in/briansugar/" class="">
+          <div  class="icon" style="background-image:url('/imgs/partners/brian.png')"></div>
+          <div class="text-center pt-2 text-primary">Brian Sugar</div>
+        </a>
+      </div>
+    </div>
+    <div class="text-secondary mt-3">
+      <h3>Portfolio</h3>
+    </div>
+    <div class="d-flex flex-wrap">
+      <?php foreach ($fund1 as $company) { ?>
         <div class="text-sans-serif company-logo text-center m-1 m-lg-4">
           <a href="<?= $company['link'] ?>" class="">
             <div  class="icon" style="background-image:url('<?= $company['img'] ?>')"></div>
             <div class="text-center pt-2 text-primary"><?= $company['name'] ?></div>
+          </a>
+        </div>
+      <?php } ?>
+    </div>
+    <div class="mt-3 text-secondary">
+      <h3>Enduring Companies</h3>
+    </div>
+    <div class="d-flex flex-wrap">
+      <?php foreach ($angel as $company) { ?>
+        <div class="text-sans-serif company-logo text-center m-1 m-lg-4">
+          <a href="<?= $company['link'] ?>" class="">
+            <div  class="icon" style="background-image:url('<?= $company['img'] ?>')"></div>
+            <div class="text-center pt-2 text-primary"><?= $company['name'] ?></div>  
           </a>
         </div>
       <?php } ?>
